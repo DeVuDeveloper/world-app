f4f0dbe0dd70221ad6742eeb9bd9c5dd727495a7
VuDej <drvu47@gmail.com>
2022-03-07 11:50:13 +0100

    added maps for countries

--- src/components/oneCountry/OneCountry.js
+++ src/components/oneCountry/OneCountry.js
@@ -1,27 +1,35 @@
 import PropTypes from 'prop-types';
+import { BsArrowRightCircle } from 'react-icons/bs';
+import CountryCode from './CountryCode';
 import './oneCountry.css';
 
 const OneCountry = ({
   countryName,
   todayConfirmed,
-}) => (
-  <li className="card">
-    <span className="card-images">
-      <img
-        className="card-img"
-        src={' '}
-        alt="country"
-        width="80"
-        height="80"
-      />
-    </span>
-    <span className="card-text">
-      <h3>{countryName}</h3>
-      <p>{todayConfirmed}</p>
-    </span>
-  </li>
-);
-
+}) => {
+  const countryImg = CountryCode[countryName];
+  const map = countryImg
+    ? `https://raw.githubusercontent.com/djaiss/mapsicon/33ba28808f8d32b5bae0ffada9cadd07073852e1/all/${countryImg.toLowerCase()}/vector.svg`
+    : '';
+  return (
+    <li className="card">
+      <span className="card-images">
+        <img
+          className="card-img"
+          src={map}
+          alt="country"
+          width="80"
+          height="80"
+        />
+        <BsArrowRightCircle className="circle" />
+      </span>
+      <span className="card-text">
+        <h3>{countryName}</h3>
+        <p>{todayConfirmed}</p>
+      </span>
+    </li>
+  );
+};
 OneCountry.propTypes = {
   countryName: PropTypes.string.isRequired,
   todayConfirmed: PropTypes.string.isRequired,
