587e13b4c1fea87589f6638a4fbc7e1ab4f1249c
VuDej <drvu47@gmail.com>
2022-03-07 12:55:53 +0100

    added id and OneCountry linked with id to page CountryInfo

--- src/components/oneCountry/OneCountry.js
+++ src/components/oneCountry/OneCountry.js
@@ -1,36 +1,49 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { BsArrowRightCircle } from 'react-icons/bs';
 import CountryCode from './CountryCode';
 import './oneCountry.css';
 
 const OneCountry = ({
+  id,
   countryName,
   todayConfirmed,
 }) => {
   const countryImg = CountryCode[countryName];
   const map = countryImg
-    ? `https://raw.githubusercontent.com/djaiss/mapsicon/33ba28808f8d32b5bae0ffada9cadd07073852e1/all/${countryImg.toLowerCase()}/vector.svg`
+    ? `https://raw.githubusercontent.com/VuDej/worldMaps/main/maps/${countryImg.toLowerCase()}/128.png`
     : '';
+
   return (
-    <li className="card">
-      <span className="card-images">
-        <img
-          className="card-img"
-          src={map}
-          alt="country"
-          width="80"
-          height="80"
-        />
-        <BsArrowRightCircle className="circle" />
-      </span>
-      <span className="card-text">
-        <h3>{countryName}</h3>
-        <p>{todayConfirmed}</p>
-      </span>
+    <li
+      key={id}
+      className="card"
+    >
+      <Link
+        to={`/${id}`}
+        style={{ textDecoration: 'none' }}
+      >
+
+        <span className="card-images">
+          <img
+            className="card-img"
+            src={map}
+            alt="country"
+            width="80"
+            height="80"
+          />
+          <BsArrowRightCircle className="circle" />
+        </span>
+        <span className="card-text">
+          <h3>{countryName}</h3>
+          <p>{todayConfirmed}</p>
+        </span>
+      </Link>
     </li>
   );
 };
 OneCountry.propTypes = {
+  id: PropTypes.string.isRequired,
   countryName: PropTypes.string.isRequired,
   todayConfirmed: PropTypes.number.isRequired,
 };
